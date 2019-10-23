07e0e1355841aeade83882999f08a0f77d851a22
matheus <matricci2011@hotmail.com>
2019-10-23 19:29:45 -0300

    Remove thunk and Redux-devtool

--- src/store.js
+++ src/store.js
@@ -1,16 +1,8 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import rootReducer from "./reducers";
 
 const initialState = {};
 
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = createStore(rootReducer, initialState);
 
 export default store;
