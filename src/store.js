27d2cb8cfafeffa437939999b21180e65ccac95b
matheus <matricci2011@hotmail.com>
2019-10-23 19:53:53 -0300

    Bring back thunk

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
 
 const initialState = {};
-
+const middleware = [thunk];
 const store = createStore(rootReducer, initialState);
 
 export default store;
