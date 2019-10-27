2d11811cfac9928c79039d3fede392d56713da26
Matheus Ricci <matricci2011@hotmail.com>
2019-10-27 18:25:00 -0300

    Fix applymiddleware

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
+
 const initialState = {};
 const middleware = [thunk];
-const store = createStore(rootReducer, initialState);
+const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
 
 export default store;
