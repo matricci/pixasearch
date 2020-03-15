b013684b00ef4744bb1afdca05a2b18609b087d3
Matheus Ricci <matricci2011@hotmail.com>
2020-03-15 18:08:34 -0300

    Fix: Centralize and use a decent border radius

--- src/components/Layout/Search.js
+++ src/components/Layout/Search.js
@@ -1,105 +1,135 @@
-import React, { useState, Fragment } from 'react'
-import { Paper, IconButton, InputBase, AppBar, Divider, Icon } from '@material-ui/core'
-import { Search as SearchIcon, Close } from '@material-ui/icons'
-import { makeStyles, fade } from '@material-ui/core/styles';
-import { searchImages, searchText } from '../../actions/search'
-import { useDispatch } from 'react-redux'
-
+import React, { useState, Fragment } from "react";
+import {
+  Paper,
+  IconButton,
+  InputBase,
+  AppBar,
+  Divider,
+  Icon
+} from "@material-ui/core";
+import { Search as SearchIcon, Close } from "@material-ui/icons";
+import { makeStyles, fade } from "@material-ui/core/styles";
+import { searchImages, searchText } from "../../actions/search";
+import { useDispatch } from "react-redux";
 
 const useStyles = makeStyles(theme => ({
-    divider: {
-        height: 'auto',
-        marginTop: '6px',
-        marginBottom: '6px',
-        marginLeft: 0
-    },
-    inputInput: {
-        marginRight: 'auto',
-        padding: theme.spacing(1, 1, 1, 1),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: 200,
-        },
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    search: {
-        color: "inherit",
-        boxShadow: 'none',
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    searchRoot: {
-        position: 'relative',
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
+  divider: {
+    height: "auto",
+    marginTop: "6px",
+    marginBottom: "6px",
+    marginLeft: 0
+  },
+  inputInput: {
+    marginRight: "auto",
+    padding: theme.spacing(1, 1, 1, 1),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
     }
-}))
+  },
+  inputRoot: {
+    color: "inherit"
+  },
+  search: {
+    color: "inherit",
+    boxShadow: "none",
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  searchRoot: {
+    position: "relative",
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    }
+  }
+}));
 
 export default function Search() {
-    const scrollTop = event => {
-        const anchor = (event.target.ownerDocument || document).querySelector('#top');
-        if (anchor) {
-            anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
-        }
-    };
-    const [text, setText] = useState('')
-    const dispatch = useDispatch()
-    const classes = useStyles()
-    return (
-        <AppBar>
-            <Paper style={{ display: 'inline-flex', width: '100%',  position: 'fixed', borderRadius: 50}}>
-                <IconButton onClick={e => {
-                    dispatch(searchImages(text.split(' ').join('+')))
-                    dispatch(searchText(text.split(' ').join('+')))
-                    scrollTop(e)
-                    }}>
-                    <SearchIcon />
-                </IconButton>
-                <Divider orientation="vertical" light className={classes.divider} variant="middle"/> 
-                <InputBase classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                }} fullWidth={true} onChange={e => {
-                    setText(e.target.value)
-                    }} value={text}/>
-            {text ? (
-                <IconButton onClick={e => {
-                    setText('')
-                    dispatch(searchImages(''))  
-                    dispatch(searchText(''))
-                    scrollTop(e)
-                }}>
-                    <Close />
-                </IconButton>
-            ) : (
-                <Fragment />
-            )}
-            </Paper>
-        </AppBar>
-    )
+  const scrollTop = event => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#top"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  };
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  return (
+    <AppBar style={{ alignContent: "center" }}>
+      <Paper
+        style={{
+          display: "inline-flex",
+          width: "-webkit-fill-available",
+          position: "fixed",
+          marginLeft: "2%",
+          marginRight: "2%"
+        }}
+      >
+        <IconButton
+          onClick={e => {
+            dispatch(searchImages(text.split(" ").join("+")));
+            dispatch(searchText(text.split(" ").join("+")));
+            scrollTop(e);
+          }}
+        >
+          <SearchIcon />
+        </IconButton>
+        <Divider
+          orientation="vertical"
+          light
+          className={classes.divider}
+          variant="middle"
+        />
+        <InputBase
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput
+          }}
+          fullWidth={true}
+          onChange={e => {
+            setText(e.target.value);
+          }}
+          value={text}
+        />
+        {text ? (
+          <IconButton
+            onClick={e => {
+              setText("");
+              dispatch(searchImages(""));
+              dispatch(searchText(""));
+              scrollTop(e);
+            }}
+          >
+            <Close />
+          </IconButton>
+        ) : (
+          <Fragment />
+        )}
+      </Paper>
+    </AppBar>
+  );
 }
