b4292cbb9c4256d39831cf4b33e24021a2c46868
Matheus Ricci <matricci2011@hotmail.com>
2020-08-06 23:39:01 -0300

    [Feature] Toggle safe search

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 
-const key = localStorage.getItem('key')
+const key = localStorage.getItem("key");
+const safeSearch = localStorage.getItem("safesearch");
+console.log(safeSearch)
+if (safeSearch == undefined) {
+  localStorage.setItem("safesearch", true);
+}
 const api = axios.create({
-    baseURL: `https://pixabay.com/api/?key=${key}&per_page=20`
-  });
-  
-  export default api;
+  baseURL: `https://pixabay.com/api/?key=${key}&safesearch=${safeSearch}&per_page=20`,
+});
+
+export default api;
--- src/components/Layout/Search.js
+++ src/components/Layout/Search.js
@@ -1,23 +1,30 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import {
   Paper,
   IconButton,
   InputBase,
   AppBar,
   Divider,
-  Icon
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogActions,
+  Button,
+  Switch,
+  FormGroup,
+  FormControlLabel,
 } from "@material-ui/core";
-import { Search as SearchIcon, Close } from "@material-ui/icons";
 import { makeStyles, fade } from "@material-ui/core/styles";
+import { Search as SearchIcon, Close, Settings } from "@material-ui/icons";
 import { searchImages, searchText } from "../../actions/search";
 import { useDispatch } from "react-redux";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   divider: {
     height: "auto",
     marginTop: "6px",
     marginBottom: "6px",
-    marginLeft: 0
+    marginLeft: 0,
   },
   inputInput: {
     marginRight: "auto",
@@ -25,11 +32,11 @@ const useStyles = makeStyles(theme => ({
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: 200
-    }
+      width: 200,
+    },
   },
   inputRoot: {
-    color: "inherit"
+    color: "inherit",
   },
   search: {
     color: "inherit",
@@ -38,15 +45,15 @@ const useStyles = makeStyles(theme => ({
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
-      width: "auto"
-    }
+      width: "auto",
+    },
   },
   searchIcon: {
     width: theme.spacing(7),
@@ -55,19 +62,19 @@ const useStyles = makeStyles(theme => ({
     pointerEvents: "none",
     display: "flex",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   searchRoot: {
     position: "relative",
     backgroundColor: fade(theme.palette.common.white, 0.15),
     "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    }
-  }
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+  },
 }));
 
 export default function Search() {
-  const scrollTop = event => {
+  const scrollTop = (event) => {
     const anchor = (event.target.ownerDocument || document).querySelector(
       "#top"
     );
@@ -76,68 +83,120 @@ export default function Search() {
     }
   };
   const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  const [checked, setChecked] = useState(true);
   const dispatch = useDispatch();
   const classes = useStyles();
+  const toggleChecked = () => {
+    setChecked((prev) => !prev);
+    console.log(checked);
+  };
+  useEffect(() => {
+    var boolSafeSearch = /true/i.test(localStorage.getItem("safesearch"));
+    setChecked(boolSafeSearch);
+    console.log(boolSafeSearch);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("safesearch", checked);
+  }, [checked]);
   return (
-    <AppBar style={{ alignContent: "center" }}>
-      <Paper
-        style={{
-          display: "inline-flex",
-          width: "-webkit-fill-available",
-          position: "fixed",
-          marginLeft: "2%",
-          marginRight: "2%"
-        }}
-      >
-        <IconButton
-          onClick={e => {
-            dispatch(searchImages(text.split(" ").join("+")));
-            dispatch(searchText(text.split(" ").join("+")));
-            scrollTop(e);
+    <Fragment>
+      <AppBar style={{ alignContent: "center" }}>
+        <Paper
+          style={{
+            display: "inline-flex",
+            width: "-webkit-fill-available",
+            position: "fixed",
+            marginLeft: "2%",
+            marginRight: "2%",
           }}
         >
-          <SearchIcon />
-        </IconButton>
-        <Divider
-          orientation="vertical"
-          light
-          className={classes.divider}
-          variant="middle"
-        />
-        <InputBase
-          classes={{
-            root: classes.inputRoot,
-            input: classes.inputInput
-          }}
-          fullWidth={true}
-          onChange={e => {
-            setText(e.target.value);
-          }}
-          value={text}
-          autoFocus={true}
-          onKeyPress={event => {
-            if (event.key === "Enter") {
+          <IconButton
+            onClick={(e) => {
               dispatch(searchImages(text.split(" ").join("+")));
               dispatch(searchText(text.split(" ").join("+")));
-              scrollTop(event);
-            }
-          }}
-        />
-        {text ? (
-          <IconButton
-            onClick={e => {
-              setText("");
-              dispatch(searchImages(""));
-              dispatch(searchText(""));
               scrollTop(e);
             }}
           >
-            <Close />
+            <SearchIcon />
           </IconButton>
-        ) : (
-          <Fragment />
-        )}
-      </Paper>
-    </AppBar>
+          <Divider
+            orientation="vertical"
+            light
+            className={classes.divider}
+            variant="middle"
+          />
+          <InputBase
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            fullWidth={true}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            value={text}
+            autoFocus={true}
+            onKeyPress={(event) => {
+              if (event.key === "Enter") {
+                dispatch(searchImages(text.split(" ").join("+")));
+                dispatch(searchText(text.split(" ").join("+")));
+                scrollTop(event);
+              }
+            }}
+          />
+          {text ? (
+            <IconButton
+              onClick={(e) => {
+                setText("");
+                dispatch(searchImages(""));
+                dispatch(searchText(""));
+                scrollTop(e);
+              }}
+            >
+              <Close />
+            </IconButton>
+          ) : (
+            <Fragment>
+              <IconButton onClick={() => setOpen(true)}>
+                <Settings />
+              </IconButton>
+            </Fragment>
+          )}
+        </Paper>
+      </AppBar>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Configuration</DialogTitle>
+        <DialogContent>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={checked}
+                  onChange={() => {
+                    toggleChecked();
+                  }}
+                />
+              }
+              label="Safe Search"
+              labelPlacement="start"
+            />
+          </FormGroup>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            size="small"
+            color="primary"
+            variant="contained"
+            onClick={() => {
+              setOpen(false);
+              location.reload();
+            }}
+          >
+            SAVE
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Fragment>
   );
 }
